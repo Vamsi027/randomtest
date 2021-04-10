65531e763ab5dee448d20c0dc072868dcb3faa1d
Vamsi027 <you@example.com>
2021-04-10 17:33:51 +0530

    Multiple users stored

--- main2.js
+++ main2.js
@@ -42,17 +42,37 @@ function onSubmit(e)
     }
     else
     {
-        localStorage.setItem('name',nameInput.value)
-        localStorage.setItem('email',emailInput.value)
+        if(localStorage.getItem('userDetails')==null){
+            localStorage.setItem('userDetails','[]')
+        }
+        
+        var old=JSON.parse(localStorage.getItem('userDetails'));
+        old.push(nameInput.value)
+        localStorage.setItem('userDetails',JSON.stringify(old));
+    
         console.log(nameInput.value);
         console.log(emailInput.value);
     }
 }
 
-window.addEventListener('DOMContentLoaded', (event) => {
+/*window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
     var name=window.localStorage.getItem('name')
     var email=window.localStorage.getItem('email')
     message.innerHTML=`<h1 color='green'>${name} and ${email} name and email from local storage</h1>`;
     
-});
+});*/
+const another=document.querySelector('#another')
+another.addEventListener('submit',onSubmited);
+function onSubmited(e)
+{
+    e.preventDefault();
+    var li=localStorage.getItem('userDetails').split("[,]")
+    const pi=document.createElement('li')
+    pi.appendChild(document.createTextNode(li[0]))
+    
+    if(users.lastElementChild==null)
+        users.appendChild(pi)
+    if(users.lastElementChild.textContent!=li)
+        users.appendChild(pi)
+}
