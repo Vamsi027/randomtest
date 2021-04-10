6db02d24befcd5a1a53cdad6fcb846eec40b56f7
Vamsi027 <you@example.com>
2021-04-10 14:54:00 +0530

    handled some events using DOM

--- main2.js
+++ main2.js
@@ -1,10 +1,47 @@
+/*
 const ul=document.querySelector('.items')
 
 ul.firstElementChild.textContent='Hello'
 ul.children[1].innerText='Brad'
 ul.lastElementChild.innerHTML='<h1>Hello</h1>'
-const btn=document.querySelector('.btn')
-btn.style.background='red'
-
-ul.firstElementChild.style.background='green'
+ul.firstElementChild.style.background='blue'
 ul.children[1].style.background='yellow'
+
+const btn=document.querySelector('.btn')
+
+btn.addEventListener('click',(e)=>
+{
+    e.preventDefault();
+    document.querySelector('body').style.background='black';
+})
+btn.addEventListener('mouseout',(e)=>
+{
+    document.querySelector('body').style.background='yellow';
+})
+btn.addEventListener('mouseover',(e)=>
+{
+    document.querySelector('body').style.background='green';
+})
+*/
+const myform=document.querySelector('#my-form')
+const nameInput=document.querySelector('#name')
+const emailInput=document.querySelector('#email')
+const message=document.querySelector('.msg')
+const users=document.querySelector('#users')
+
+myform.addEventListener('submit',onSubmit);
+function onSubmit(e)
+{
+    e.preventDefault();
+    if(nameInput.value===''||emailInput.value==='')
+    {
+        message.classList.add('error')
+        message.innerHTML='Please enter all the fields';
+        setTimeout(()=>message.remove(),3000)
+    }
+    else
+    {
+        console.log(nameInput.value);
+        console.log(emailInput.value)
+    }
+}
