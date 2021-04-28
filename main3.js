001176756bb6a8a90abb6c69ca91c1e236e87ef3
Vamsi027 <you@example.com>
2021-04-28 14:39:18 +0530

    binary search tree

--- main3.js
+++ main3.js
@@ -1,9 +1,63 @@
-var ul=document.querySelector('#users')
-
-var l=JSON.parse(localStorage.getItem('userDetails'))
-for(let i of l)
+class Node
 {
-    var pi=document.createElement('li')
-    pi.innerHTML=`<p>${i}</p><button class='btn'>Delete</button><button  class='btn'>Edit</button>`
-    ul.appendChild(pi)
+    constructor(data)
+    {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+class BinarySearchTree
+{
+    constructor()
+    {
+        this.root = null;
+    }
+  
+    insert(data)
+    {
+    var newNode = new Node(data);
+                      
+    if(this.root === null)
+        this.root = newNode;
+    else
+        this.insertNode(this.root, newNode);
+    }
+  
+    insertNode(node, newNode)
+    {
+     
+       if(newNode.data < node.data)
+       {
+        
+        if(node.left === null)
+            node.left = newNode;
+        else
+            this.insertNode(node.left, newNode); 
+       }
+  
+        else
+        {
+          if(node.right === null)
+             node.right = newNode;
+          else
+            this.insertNode(node.right,newNode);
+       }
+    }
+    
+    search(node, data)
+    {
+   
+       if(node === null)
+         return null;
+  
+       else if(data < node.data)
+         return this.search(node.left, data);
+  
+       else if(data > node.data)
+         return this.search(node.right, data);
+  
+       else
+          return node;
+    }
 }
