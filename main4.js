72dd52d2712d5fd43e5c4c58f622fc484014908d
Vamsi027 <you@example.com>
2021-05-01 11:22:55 +0530

    get user details

--- main4.js
+++ main4.js
@@ -1,21 +1,32 @@
 const formElement=document.querySelector('#form-element');
 formElement.addEventListener('submit',onSubmit);
 
+// function onSubmit(e)
+// {
+//     e.preventDefault();
+//     let nm=document.querySelector('#name');
+//     let em=document.querySelector('#email');
+//     let ph=document.querySelector('#phoneNo')    
+    
+//     axios({
+//         method:'post',
+//         url:'https://crudcrud.com/api/8bad69f475834e5fbc80c7de2635c5f4/unicorns', 
+//         data:{
+//             name: nm.value,
+//             email:em.value,
+//             phone:ph.value
+//         }
+//         })
+//         .then(res => showOutput(res))
+//         .catch(err => console.error(err));
+// }
 function onSubmit(e)
 {
-    e.preventDefault();
-    let nm=document.querySelector('#name');
-    let em=document.querySelector('#email');
-    let ph=document.querySelector('#phoneNo')    
+    e.preventDefault();   
     
     axios({
-        method:'post',
-        url:'https://crudcrud.com/api/8bad69f475834e5fbc80c7de2635c5f4/unicorns', 
-        data:{
-            name: nm.value,
-            email:em.value,
-            phone:ph.value
-        }
+        method:'get',
+        url:'https://crudcrud.com/api/8bad69f475834e5fbc80c7de2635c5f4/unicorns'
         })
         .then(res => showOutput(res))
         .catch(err => console.error(err));
