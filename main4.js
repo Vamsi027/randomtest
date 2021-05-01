135608e2eab4ee887362b51e9cfeafd30cf4ff55
Vamsi027 <you@example.com>
2021-05-01 11:32:04 +0530

    Update User Details

--- main4.js
+++ main4.js
@@ -31,18 +31,36 @@ formElement.addEventListener('submit',onSubmit);
 //         .then(res => showOutput(res))
 //         .catch(err => console.error(err));
 // }
+// function onSubmit(e)
+// {
+//     e.preventDefault();   
+    
+//     axios({
+//         method:'delete',
+//         url:'https://crudcrud.com/api/8bad69f475834e5fbc80c7de2635c5f4/unicorns/userId'
+//         })
+//         .then(res => showOutput(res))
+//         .catch(err => console.error(err));
+// }
 function onSubmit(e)
 {
-    e.preventDefault();   
+    e.preventDefault();
+    let nm=document.querySelector('#name');
+    let em=document.querySelector('#email');
+    let ph=document.querySelector('#phoneNo')    
     
     axios({
-        method:'delete',
-        url:'https://crudcrud.com/api/8bad69f475834e5fbc80c7de2635c5f4/unicorns/userId'
+        method:'put',
+        url:'https://crudcrud.com/api/8bad69f475834e5fbc80c7de2635c5f4/unicorns/userId', 
+        data:{
+            name: nm.value,
+            email:em.value,
+            phone:ph.value
+        }
         })
         .then(res => showOutput(res))
         .catch(err => console.error(err));
 }
-
 function showOutput(res) {
     document.querySelector('#studentDetails').innerHTML =
     `<pre>${JSON.stringify(res.data, null, 2)}</pre>`;  
